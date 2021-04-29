14c4714547c74a60645bca5520326868a0faa029
Murodbek <bekbruno99@gmail.com>
2021-04-30 02:53:14 +0500

    adf

--- client/src/comps/Users/Users.js
+++ client/src/comps/Users/Users.js
@@ -144,7 +144,7 @@ const Users = () => {
         color="#00BFFF"
         height={50}
         width={50}
-        timeout={3000} //3 secs
+        timeout={1400} 
       />
       <MaterialTable
         title={productInfo.name}
