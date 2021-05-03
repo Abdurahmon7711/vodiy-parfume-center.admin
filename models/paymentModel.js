782a8c19b0697dac35c66ec23e2dfa80583e5d7a
AbdulazizProgrammer <Abdulazizprogrammer@gmail.com>
2021-05-03 11:30:05 +0500

    some works are finished

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -22,6 +22,10 @@ const paymentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    address: {
+      type: String,
+      required: true,
+    },
     cart: {
       type: Array,
       default: [],
